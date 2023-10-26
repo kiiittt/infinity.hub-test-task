6f139321ffcb2d9c68618fc301eb42537cb84799
kiiittt <117594092+kiiittt@users.noreply.github.com>
2023-10-26 13:00:30 +0000

    Deploying to gh-pages from @ kiiittt/untitled@f298f12c7d56ed517caa7ad122b710a88729680d 🚀

---
+++ index.42999bc2.js
@@ -0,0 +1,2 @@
+var t=new Date;t.setDate(t.getDate()-5);var e=t.toLocaleDateString();document.getElementById("date-container").textContent=e;
+//# sourceMappingURL=index.42999bc2.js.map
---
+++ index.f1275a08.js
@@ -0,0 +1,2 @@
+!function(){var t=new Date;t.setDate(t.getDate()-5);var e=t.toLocaleDateString();document.getElementById("date-container").textContent=e}();
+//# sourceMappingURL=index.f1275a08.js.map
